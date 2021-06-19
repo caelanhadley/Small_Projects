fbc3d400159035620995d1d69b129aa6732046c4
caelanhadley <caelanhadley@gmail.com>
2021-06-19 00:21:23 -0500

    Rollback

---
+++ Java/Collections/MusicPlayer.java
@@ -0,0 +1,11 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+    /*
+    * List <-- is an interface, i.e. it can be interchanged with different types.
+    * List --> ArrayList, LinkedList, Vector
+    */
+    List<Song> playlist = new ArrayList<Song>();
+}
